85052425c913312a33db16386037ecd0af3d610b
Lucemans <luc@lucemans.nl>
2022-03-29 14:06:27 +0200

    Introduce Caching and Basic Auth

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,11 @@
 import { sign } from 'jsonwebtoken';
 
+import { CACHE } from '../cache';
 import { DB } from '../database';
 import { generateSnowflake } from '../routes/api';
 import { GithubUser } from '../types/GithubUser.type';
 import { Owner } from '../types/Owner.type';
+import { log } from '../util/logging';
 
 export const getUserByGithub = (github_id: string) =>
     DB.selectOneFrom('owners', ['user_id'], { github_id });
@@ -26,3 +28,17 @@ export const signToken = (user_id: string) =>
         },
         process.env.SIGNAL_MASTER
     );
+
+export const getLoginSessionByState = async (state: string) => {
+    const session = await CACHE.get('sedge-auth-' + state);
+
+    log.debug('finding ' + state, session);
+
+    if (session) return;
+
+    return session;
+};
+
+export const setPSKfromState = async (code: string, psk: string) => {
+    await CACHE.set('sedge-auth-' + code, psk, { EX: 60 * 2 });
+};
