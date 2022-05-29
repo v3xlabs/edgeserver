91815832538c6e31f96dbffccf6f77990fd23fef
Svemat01 <jakob.helgesson@gmail.com>
2022-05-29 23:39:02 +0200

    Update domains/ls endpoint

--- src/routes/api/domains/ls.ts
+++ src/routes/api/domains/ls.ts
@@ -2,14 +2,19 @@ import { FastifyPluginAsync } from 'fastify';
 import { generateSunflake } from 'sunflake';
 
 import { DB } from '../../../database';
+import { useAuth } from '../../../util/http/useAuth';
 
 export const generateSnowflake = generateSunflake();
 
 export const DomainLsRoute: FastifyPluginAsync = async (router, options) => {
     router.get('/', async (_request, reply) => {
+        const authData = await useAuth(_request, reply);
+
+        if (typeof authData !== 'string') return reply.send(authData);
+
         reply.send(
             await DB.selectFrom('sites', '*', {
-                owner: '123456789',
+                owner: authData,
             })
         );
     });
