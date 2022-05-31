576a9256833879d149760da5b717aca2273aae6a
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2022-05-31 10:33:19 -0400

    AppEntryRoute select from db

--- src/routes/api/apps/[app]/index.ts
+++ src/routes/api/apps/[app]/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsync } from 'fastify';
 import { generateSunflake } from 'sunflake';
 
+import { DB } from '../../../../database';
 import { Auth } from '../../../../middleware/auth';
 import { AppDeploysRoute } from './deploys';
 
@@ -14,8 +15,13 @@ export type AppIDParameters = {
 export const AppEntryRoute: FastifyPluginAsync = async (router, _options) => {
     router.register(Auth);
 
-    router.get<AppIDParameters>('/', (_request, reply) => {
-        reply.send('stick');
+    router.get<AppIDParameters>('/', async (_request, reply) => {
+        const parameters = _request.params;
+        const app = await DB.selectOneFrom('applications', '*', {
+            app_id: parameters.app_id,
+        });
+
+        reply.send(app);
     });
 
     router.register(AppDeploysRoute, { prefix: '/deploys' });
