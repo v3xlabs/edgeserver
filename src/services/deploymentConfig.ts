c0100a1ae0100a37eb19f6cf69271a8dcba4225c
Svemat01 <jakob.helgesson@gmail.com>
2022-07-02 13:42:04 +0200

    use routing config

--- src/services/deploymentConfig.ts
+++ src/services/deploymentConfig.ts
@@ -1,29 +1,30 @@
 import { DB } from '../database';
-import { defaultEdgeRcConfig, EdgeRcConfig } from '../types/ConfigFile.type';
+import { defaultEdgeRcConfig, RoutingConfig } from '../types/ConfigFile.type';
 import { useData } from '../util/useData';
 
 const getRoutingConfigFromRedis =
-    (base_url: string) => async (): Promise<EdgeRcConfig | undefined> => {
+    (base_url: string) => async (): Promise<RoutingConfig | undefined> => {
         // TODO get config from redis
         return undefined;
     };
 
 const getRoutingConfigFromBaseDB =
-    (deploy_id: string) => async (): Promise<EdgeRcConfig | undefined> => {
+    (deploy_id: string) => async (): Promise<RoutingConfig | undefined> => {
         const data = await DB.selectOneFrom('deployment_configs', ['routing'], {
             deploy_id,
         });
 
         // TODO validate config
-        return data ? (JSON.parse(data.routing) as EdgeRcConfig) : undefined;
+        return data ? (JSON.parse(data.routing) as RoutingConfig) : undefined;
     };
 
 export const getRoutingConfig = async (deploy_id: string) => {
-    return (
-        (await useData<EdgeRcConfig>(
+    return {
+        ...defaultEdgeRcConfig.routing,
+        ...(await useData<RoutingConfig>(
             'config_' + deploy_id,
             getRoutingConfigFromRedis(deploy_id),
             getRoutingConfigFromBaseDB(deploy_id)
-        )) || defaultEdgeRcConfig
-    );
+        )),
+    };
 };
