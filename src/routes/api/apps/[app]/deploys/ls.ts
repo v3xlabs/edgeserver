3b78a6e27eb453f92a7302f28374c8037892f5fc
Luc <luc@lucemans.nl>
2022-07-21 15:39:05 +0200

    Introduce Total (#78)
    * Introduce Total
    
    * Introduce Deployment Pagination

--- src/routes/api/apps/[app]/deploys/ls.ts
+++ src/routes/api/apps/[app]/deploys/ls.ts
@@ -9,7 +9,14 @@ import { AppIDParameters } from '..';
 export const generateSnowflake = generateSunflake();
 
 export const DeploysLsRoute: FastifyPluginAsync = async (router, _options) => {
-    router.get<AppIDParameters>('/', async (_request, reply) => {
+    router.get<
+        AppIDParameters & {
+            Querystring: {
+                offset: string;
+                limit: number;
+            };
+        }
+    >('/', async (_request, reply) => {
         const { user_id: _user_id, permissions } = await useAuth(
             _request,
             reply
@@ -24,7 +31,11 @@ export const DeploysLsRoute: FastifyPluginAsync = async (router, _options) => {
                 {
                     app_id: _request.params.app_id,
                 },
-                'ORDER BY deploy_id DESC LIMIT 50'
+                (_request.query.offset
+                    ? 'and deploy_id < ' + _request.query.offset
+                    : '') +
+                    ' ORDER BY deploy_id DESC LIMIT ' +
+                    (_request.query.limit || 50)
             )
         );
     });
