a7270f44d55964f7f452dd9a83228489fa9c4f17
Lucemans <luc@lucemans.nl>
2022-08-18 23:20:31 +0200

    Update CI/CD & Introduce CLI

---
+++ packages/cli/README.md
@@ -0,0 +1,1 @@
+# sigcli-node
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,10 @@
+import { createLogger } from '@lvksh/logger';
+
+const log = createLogger({
+    debug: 'DEBUG',
+    info: 'INFO',
+});
+
+console.log('hi');
+log.debug('hi');
+log.debug('never gonna give u up');
