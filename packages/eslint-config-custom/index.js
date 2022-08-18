f28539b07bab299a54eab458211615c61cce8a81
Lucemans <luc@lucemans.nl>
2022-08-19 01:15:52 +0200

    Testing

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,6 +3,10 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
-    "unused-imports/no-unused-imports": "off"
+    "unused-imports/no-unused-imports": "off",
+    "unicorn/consistent-function-scoping": "off",
+    "unicorn/no-nested-ternary": "off",
+    "no-undef": "off",
+    "sonarjs/no-duplicate-string": "off"
   },
 };
