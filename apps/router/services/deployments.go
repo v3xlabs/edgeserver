e26699cc18389183f7cb075cbe5fa1e162ed3e16
Svemat01 <jakob.helgesson@gmail.com>
2022-11-03 16:44:46 +0100

    Fix Get Deployments

--- apps/router/services/deployments.go
+++ apps/router/services/deployments.go
@@ -1,17 +1,16 @@
 package services
 
 import (
+	"github.com/scylladb/gocqlx/v2/qb"
 	"github.com/v3xlabs/edgeserver/apps/router/db"
 	"github.com/v3xlabs/edgeserver/apps/router/models"
 )
 
 func GetDeploymentData(deployId int64) (models.DeploymentStruct, error) {
 	session := db.Get()
-	data := models.DeploymentStruct{
-		DeployId: deployId,
-	}
+	data := models.DeploymentStruct{}
 
-	q := session.Query(models.Deployments.Get()).BindStruct(data)
+	q := session.Query("SELECT * FROM deployments WHERE deploy_id=?", []string{"deploy_id"}).BindMap(qb.M{"deploy_id": deployId})
 	err := q.GetRelease(&data)
 
 	return data, err
