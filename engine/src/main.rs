e93f837b0b4d3ef807d9a41aa29dbc2c5e7b7876
Luc <luc@lucemans.nl>
2025-01-01 23:52:18 +0100

    Introduce basic env variables

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,21 +1,26 @@
-use tracing::info;
+use state::AppState;
+use tracing::{error, info};
 
+pub mod middlewares;
 pub mod models;
 pub mod routes;
 pub mod state;
-pub mod middlewares;
 
 #[async_std::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
-    println!("Hello, world!");
-
     tracing_subscriber::fmt::init();
 
     info!("Starting Edgerouter");
 
-    let state = state::AppState {};
+    let state = match AppState::new().await {
+        Ok(state) => state,
+        Err(error) => {
+            error!("Failed to load environment variables: {}", error);
+            return;
+        }
+    };
 
     routes::serve(state).await;
 }
--- engine/src/state.rs
+++ engine/src/state.rs
@@ -1,1 +1,31 @@
-pub struct AppState {}
+use config::{Config, Environment};
+use dotenvy::var;
+use serde::Deserialize;
+
+pub struct AppState {
+    config: AppConfig,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AppConfig {
+    /// Information
+    pub database_url: String,
+    pub s3_endpoint_url: String,
+    pub s3_region: String,
+    pub s3_bucket_name: String,
+    pub s3_access_key: String,
+    pub s3_secret_key: String,
+}
+
+impl AppState {
+    pub async fn new() -> Result<Self, config::ConfigError> {
+        let config = Config::builder()
+            .add_source(Environment::default())
+            .build()?;
+
+        // Deserialize into the AppConfig struct
+        let config: AppConfig = config.try_deserialize()?;
+
+        Ok(Self { config })
+    }
+}
