3d452f2524b8c8b27fc58064bf2fbacdb286b55e
Luc van Kampen <luc@lucemans.nl>
2025-02-13 07:35:47 +0100

    Update color and theme (#189)
    Co-authored-by: waiscodes <b.wais@outlook.com>

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,7 +6,6 @@ import { createRouter, RouterProvider } from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { preflightAuth } from '@/api';
 // Import the generated route tree
 import { routeTree } from '@/routeTree.gen';
 
