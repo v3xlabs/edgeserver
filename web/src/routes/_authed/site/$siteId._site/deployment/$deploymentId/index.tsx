d42361516b9229b144c3b9ef9a43809b166e4624
Luc <luc@lucemans.nl>
2025-04-02 01:21:30 +0200

    Update ipfs url format

--- web/src/routes/_authed/site/$siteId._site/deployment/$deploymentId/index.tsx
+++ web/src/routes/_authed/site/$siteId._site/deployment/$deploymentId/index.tsx
@@ -144,6 +144,12 @@ function RouteComponent() {
 }
 
 const combineIpfsClusterUrl = (cluster_url: string, cid: string) => {
+    const DELIMITER = '%CID%';
+
+    if (cluster_url.includes(DELIMITER)) {
+        return cluster_url.replace(DELIMITER, cid);
+    }
+
     const cluster_url_suffixed_with_slash = cluster_url.endsWith('/')
         ? cluster_url
         : `${cluster_url}/`;
