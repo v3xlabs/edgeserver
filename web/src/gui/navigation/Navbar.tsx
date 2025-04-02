a433a1559c9e2d9b136acf3072e65d210d2c912a
waiscodes <waiscodes@gmail.com>
2025-04-01 21:19:10 -0600

    User icon in nav

--- web/src/gui/navigation/Navbar.tsx
+++ web/src/gui/navigation/Navbar.tsx
@@ -5,6 +5,7 @@ import { FiServer } from 'react-icons/fi';
 import { useMe } from '@/api';
 import { authStore } from '@/api/auth/store';
 import { Avatar, Button } from '@/components';
+import { AvatarGradient } from '@/components/avatar/gradient';
 
 import {
     DropdownMenu,
@@ -20,18 +21,19 @@ import { ThemeSwitcher } from './ThemeSwitcher';
 
 const UserProfile = () => {
     const { data: me } = useMe();
+    const firstLetter = me?.name?.[0]?.toUpperCase() || '?';
 
     return (
         <DropdownMenu>
             <div className="flex h-full items-end">
                 <div className="flex h-full w-fit items-center px-2 group-hover:bg-black/10">
-                    <DropdownMenuTrigger>
-                        <span>{me?.name}</span>
-                        {/* <AvatarOrGradient
-                    src={avatar}
-                    hash={user || ''}
-                    className="ml-2 size-8 overflow-hidden rounded-full bg-white"
-                /> */}
+                    <DropdownMenuTrigger className="flex items-center gap-2">
+                        <div className="relative size-8 overflow-hidden rounded-full">
+                            <AvatarGradient s={me?.user_id || ''} />
+                            <div className="absolute inset-0 flex items-center justify-center font-medium text-white">
+                                {firstLetter}
+                            </div>
+                        </div>
                     </DropdownMenuTrigger>
                 </div>
                 <DropdownMenuPortal>
@@ -82,7 +84,7 @@ export const Navbar = () => {
     return (
         <div className="w-full">
             <div className="relative z-30 h-14 w-full">
-                <div className="bg-default z-10 h-14 w-full border-b px-4">
+                <div className="z-10 h-14 w-full border-b bg-default px-4">
                     <div className="w-container-dynamic flex h-full items-center justify-between px-6">
                         <div className="flex h-full">
                             <div className="relative h-full w-8">
