2953f388d04de05aba8e466fa480f79244b7c150
Luc <luc@lucemans.nl>
2025-04-07 06:49:18 +0200

    Update build

--- engine/build.rs
+++ engine/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    build_info_build::build_script();
+    let x = build_info_build::build_script();
+    let y = x.build();
+
+    let git_info = y.version_control.map(|v| v.git().cloned());
+    println!("Git information: {:?}", git_info);
 }
